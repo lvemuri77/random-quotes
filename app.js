254c98e8f23eaff98fcc55be3da97eac947c69d0
lvemuri77 <lvemuri77@gmail.com>
2016-07-18 21:05:22 -0400

    updated twitter posts

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-
+var txt=document.getElementById('txt').innerHTML;
 
 $(document).ready(function(){
+	var photocaption;
 	$("#getQuote").on("click",function(){
 		var randomNum = Math.ceil(Math.random()*20);
-
-		$.getJSON("http://startupquote.com/api/read/json?filter=text&start="+randomNum+"&callback=?", function(data) {
+		$.getJSON("https://startupquote.com/api/read/json?filter=text&start="+randomNum+"&callback=?", function(data) {
 			$.each(data.posts, function(i, posts){
-				var photocaption = this["photo-caption"];
+				photocaption = this["photo-caption"];
 				$('#quoted-text >p').remove();
 
 				$('<p>' + photocaption + '<\/p>').appendTo('#quoted-text');
+
 				if ( i == 0 ) return false;
 			});
 
 	});
 	});
-	});
 
+	$('.button').on('click',function(){
+		window.open('https://twitter.com/intent/tweet?hashtags=&text='+encodeURIComponent (txt));
+
+	})
+	});
